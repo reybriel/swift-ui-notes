f7721ec3592d9d53cb4c767cf2d2853596e3cec7
Gabriel Reynoso <gbr.reynoso@icloud.com>
2019-09-24 23:50:40 -0300

    Create note creation upstream

--- NotesApp/Common/Utils/AlertFeedback.swift
+++ NotesApp/Common/Utils/AlertFeedback.swift
@@ -7,4 +7,13 @@ extension AlertFeedback {
     static let empty: AlertFeedback = .init(title: "", message: "")
     static let attemptToCreateNoteWithoutTitle: AlertFeedback = .init(title: "Unable to create Note",
                                                                       message: "No title was provided")
+    static var unknownError: AlertFeedback {
+        .error(message: "Something wrong happened!")
+    }
+}
+
+extension AlertFeedback {
+    private static func error(message: String) -> AlertFeedback {
+        .init(title: "Oops!", message: message)
+    }
 }
--- NotesApp/UseCase/CreateNoteWithTitleUseCase.swift
+++ NotesApp/UseCase/CreateNoteWithTitleUseCase.swift
@@ -1,3 +1,4 @@
+import Foundation
 import Combine
 
 typealias CreateNoteWithTitleUseCasePresenter = CanShowSuccess & CanShowAlertFeedback
@@ -5,12 +6,36 @@ typealias CreateNoteWithTitleUseCasePresenter = CanShowSuccess & CanShowAlertFee
 struct CreateNoteWithTitleUseCase: CanRun {
     let title: String
     unowned let presenter: CreateNoteWithTitleUseCasePresenter & AnyObject
-
+    
     func run() {
         if title.isEmpty {
             presenter.show(feedback: .attemptToCreateNoteWithoutTitle)
         } else {
-            presenter.showSuccess()
+            createNoteUpstream()
         }
+    }
+    
+    @discardableResult
+    private func createNoteUpstream() -> AnyCancellable {
+        Future(createNote)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { event in
+                if case .failure(_) = event {
+                    self.presenter.show(feedback: .unknownError)
+                }
+            }, receiveValue: {
+                self.presenter.showSuccess()
+            })
+    }
+    
+    private func createNote(completion: @escaping (Result<Void, Error>) -> Void) {
+        let _ = Timer.publish(every: 2.0, on: .main, in: .default)
+            .autoconnect()
+            .first()
+            .map({ _ in () })
+            .subscribe(on: DispatchQueue.global())
+            .sink(receiveValue: {
+                completion(.success(()))
+            })
     }
 }
