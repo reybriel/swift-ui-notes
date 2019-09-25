d789dd06d8b8c10e872d773dc7ded057c78d5a0c
Gabriel Reynoso <gbr.reynoso@icloud.com>
2019-09-25 12:37:22 -0300

    Remove note view models mocks

--- NotesApp/Model/Note.swift
+++ NotesApp/Model/Note.swift
@@ -24,20 +24,3 @@ extension Note {
         )
     }
 }
-
-#if DEBUG
-
-extension NoteViewModel {
-    static let all: [NoteViewModel] = [
-        
-        .init(id: UUID().uuidString,
-              title: "Chocolate Cake",
-              content: "Ingredients: Chocolate, Sugar..."),
-        
-        .init(id: UUID().uuidString,
-              title: "Shopping List",
-              content: "Chocolate, Sugar, Salt...")
-    ]
-}
-
-#endif
