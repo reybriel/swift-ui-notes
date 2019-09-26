16123a4878e80e7623f00b89d895cb8d0fcec4e9
Gabriel Reynoso <gbr.reynoso@icloud.com>
2019-09-26 09:03:58 -0300

    Create view model for NoteListRow

--- NotesApp/Scenes/List/Row/NoteListRow.swift
+++ NotesApp/Scenes/List/Row/NoteListRow.swift
@@ -3,16 +3,16 @@ import SwiftUI
 struct NoteListRow: View {
     static let lineLimit: Int = 1
 
-    let note: NoteViewModel
+    let viewModel: NoteListRowModel
     
     var body: some View {
         HStack {
             VStack(alignment: .leading) {
-                Text(note.title)
+                Text(viewModel.noteTitle)
                     .font(.headline)
-                if !note.content.isEmpty {
+                if viewModel.isShowingContent {
                     Spacer()
-                    Text(note.content)
+                    Text(viewModel.noteContent)
                         .font(.subheadline).italic()
                         .lineLimit(NoteListRow.lineLimit)
                 }
--- NotesApp/Scenes/List/Row/NoteListRowFactory.swift
+++ NotesApp/Scenes/List/Row/NoteListRowFactory.swift
@@ -2,6 +2,6 @@ import SwiftUI
 
 enum NoteListRowFactory {
     static func make(note: NoteViewModel) -> some View {
-        NoteListRow(note: note)
+        NoteListRow(viewModel: NoteListRowModel(note: note))
     }
 }
