44679fb6e14f6a9bf38d63d9118d519ca24717b3
Gabriel Reynoso <gbr.reynoso@icloud.com>
2019-10-10 08:20:07 -0300

    Reinstall Bundler to fix Gemfile.lock error at CI

--- scripts/run_slather_ci.sh
+++ scripts/run_slather_ci.sh
@@ -1,2 +1,12 @@
+if [[ -f Gemfile.lock ]]; then
+    GEM_BUNDLER_VERSION=$(grep -A1 -E -i -w '(BUNDLED WITH){1,1}' Gemfile.lock | grep -E -i -w "[0-9\.]{1,}" | xargs)
+	CURRENT_BUNDLER_VERSION=$(bundle --version | grep -o -E -i -w "[0-9\.]{1,}" | xargs)
+
+    if [[ $GEM_BUNDLER_VERSION != $CURRENT_BUNDLER_VERSION ]]; then
+        gem uninstall bundler --force --executables
+        gem install bundler --force
+    fi
+fi
+
 bundle install
 bundle exec slather coverage --show --html
