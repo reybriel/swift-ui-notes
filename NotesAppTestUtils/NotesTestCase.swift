6b27f6f6ca6ca16e779e4890ecda9f19f7c38a8c
Gabriel Reynoso <gbr.reynoso@icloud.com>
2019-10-15 07:54:12 -0300

    Create folder for common test resources

---
+++ NotesAppTestUtils/NotesTestCase.swift
@@ -0,0 +1,13 @@
+import XCTest
+import struct Foundation.TimeInterval
+
+open class NotesTestCase: XCTestCase {
+
+    public func expect(_ expectation: XCTestExpectation, tolerance: TimeInterval = 0.02) {
+        wait(for: [expectation], timeout: tolerance)
+    }
+
+    public func fail(message: String = "") {
+        XCTFail(message)
+    }
+}
