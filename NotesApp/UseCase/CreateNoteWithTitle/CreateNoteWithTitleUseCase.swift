b0d34b4cbe3262da6907fdd2a9598fc1f3bfc777
Gabriel Reynoso <gbr.reynoso@icloud.com>
2019-10-15 09:08:38 -0300

    Remove complexity at CreateNoteWithTitleUseCase

--- NotesApp/UseCase/CreateNoteWithTitle/CreateNoteWithTitleUseCase.swift
+++ NotesApp/UseCase/CreateNoteWithTitle/CreateNoteWithTitleUseCase.swift
@@ -7,18 +7,17 @@ struct CreateNoteWithTitleUseCase: CanRun {
     let title: String
     unowned let presenter: CreateNoteWithTitleUseCasePresenter & AnyObject
     unowned let gateway: CreateNoteWithTitleGateway
-    
+
     func run() {
         if title.isEmpty {
             presenter.show(feedback: .attemptToCreateNoteWithoutTitle)
         } else {
-            createNoteSaveUpstream()
+            saveNote()
         }
     }
-    
-    @discardableResult
-    private func createNoteSaveUpstream() -> AnyCancellable {
-        Future(createNote)
+
+    private func saveNote() {
+        let _ = gateway.createSaveUpstream(noteTitle: title)
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { event in
                 if case .failure(_) = event {
@@ -26,18 +25,6 @@ struct CreateNoteWithTitleUseCase: CanRun {
                 }
             }, receiveValue: {
                 self.presenter.showSuccess()
-            })
-    }
-
-    private func createNote(completion: @escaping (Result<Void, Error>) -> Void) {
-        let _ = gateway.createSaveUpstream(noteTitle: title)
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { event in
-                if case let .failure(error) = event {
-                    completion(.failure(error))
-                }
-            }, receiveValue: {
-                completion(.success(()))
             })
     }
 }
